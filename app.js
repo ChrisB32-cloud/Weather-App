84168f85eb69a13e4a5f89b74952b70caeaed7e3
ChrisBrownTheGuy <ChrisBrownTheGuy@users.noreply.github.com>
2020-04-06 16:01:46 -0500

    These are the files for the weather app

---
+++ app.js
@@ -0,0 +1,112 @@
+// Init storage
+const storage = new Storage(); 
+// Get stored location data
+const weatherLocation = storage.getLocationData();
+// Init weather object
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
+// Init UI
+const ui = new UI();
+
+// Get weather om DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// Change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    const country = document.getElementById('country').value;
+
+    // Change location
+    weather.changeLocation(city, country);
+
+    // Set Location in Local st
+    storage.setLocationData(city, country);
+
+    // Get and display weather
+    getWeather();
+
+    // Close modal
+    // $('#locModal').modal('hide');
+});
+
+
+
+
+function getWeather(){
+    weather.getWeather()
+        .then(results => {
+            ui.paint(results);
+        })
+        .catch(err => console.log(err));
+}
+
+// Refactor
+function convertToFarenhit_FeelsLike(tempK) {
+    if(tempK < (0)) {
+        return 'below absolute zero (0 K)';
+    } else {
+        let farin = 0;
+        let roundFarin = 0;
+
+        farin = (tempK*(9/5)) - 459.67;
+        roundFarin = Math.round(farin);
+        return roundFarin;
+    }
+}
+
+// combin both funtions/ Same calculation
+
+// Refactor
+// function convertFeelsLike(tempK) {
+//     if(tempK < (0)) {
+//         return 'below absolute zero (0 K)';
+//     } else {
+//         let feel = 0;
+//         let roundFeel = 0;
+
+//         feel = (tempK*(9/5)) - 459.67;
+//         roundFeel = Math.round(feel);
+//         return roundFeel;
+//     }
+// }
+
+// Converting meters per second to miles per hour
+function metersPerSecondToMilesPerHour(milePer) {
+    let mileS = 0;
+    let mileH = 0;
+    let roundMileH = 0;
+
+    mileS = milePer * 2.236936;
+    mileH = mileS;
+    roundMileH = Math.round(mileH);
+    return roundMileH;
+}
+
+// Convert degree to Wind Dirction
+function windDircFromDeg(degree) {
+    if(degree >= 1 && degree <= 44){
+        return 'North East';
+    } else if(degree == 45){
+        return 'North';
+    } else if(degree >= 46 && degree <= 179){
+        return 'North West';
+    } else if(degree == 180){
+        return 'West';
+    } else if(degree >= 181 && degree <= 269){
+        return 'South West';
+    } else if(degree == 270){
+        return 'South';
+    } else if(degree >= 271 && degree <= 314){
+        return 'South East';
+    } else {
+        return 'East';
+    }
+}
+
+// Convert hPa to Psi
+function convertPhaToPsi(hPa) {
+    let psi = hPa * 0.0145038;
+    let roundPsi = Math.round(psi);
+
+    return roundPsi;
+}
---
+++ ui.js
@@ -0,0 +1,30 @@
+class UI {
+    constructor() {
+        this.location = document.getElementById('w-location');
+        this.desc = document.getElementById('w-desc');
+        this.string = document.getElementById('w-string'); 
+        this.details = document.getElementById('w-details');
+        this.humidity = document.getElementById('w-humidity');
+        this.icon = document.getElementById('w-icon');
+        this.feelsLike = document.getElementById('w-feels-like');
+        this.pressure = document.getElementById('w-pressure');
+        this.windSpeed = document.getElementById('w-wind-speed');
+        this.windDirection = document.getElementById('w-wind-direction');
+    }
+    paint(weather) {
+        this.location.textContent = `${weather.name}, ${weather.sys.country}`;
+        this.desc.textContent = weather.weather[0].main;
+        this.string.textContent = `${convertToFarenhit_FeelsLike(weather.main.temp)}\u00B0F`;
+
+// Trying to make this icon show but not sure how to with this api
+        // this.icon.textContent = weather.weather[0].icon;
+// I know textContent isn't the right method but having trouble finding the right route
+
+        this.humidity.textContent = `Humidty: ${weather.main.humidity}%`;
+        this.feelsLike.textContent = `Feels like: ${convertToFarenhit_FeelsLike(weather.main.feels_like)}\u00B0F`;
+        this.pressure.textContent = `Pressure: ${convertPhaToPsi(weather.main.pressure)} Per in.`;
+        this.windSpeed.textContent = `Wind Speed: ${metersPerSecondToMilesPerHour(weather.wind.speed)} mph`;
+        this.windDirection.textContent = `Wind Direction: ${windDircFromDeg(weather.wind.deg)}`;
+
+    }
+}
